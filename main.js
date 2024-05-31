c98f266a105899bb67fa8d69aa7d928759e68f86
Christian Adler <chris.adler@gmx.de>
2024-05-31 08:10:24 +0200

    add zoom per click

--- main.js
+++ main.js
@@ -1,10 +1,14 @@
+const zoomDiv = document.getElementById("zoom");
+const translateXDiv = document.getElementById("translateX");
+const translateYDiv = document.getElementById("translateY");
+const timesDiv = document.getElementById("times");
 const canvas = document.getElementById("canvas");
 const ctx = canvas.getContext("2d");
 
-const zoom = 100000; // 2000;
-const times = 2000;
-const scaleX = 0.233; //0.12
-const scaleY = 0.655; // 0.82
+let zoom = 1; // 100000 // 2000;
+let times = 10; // 2000;
+let translateX = 0; // 0.233; //0.12
+let translateY = 0; // 0.655; // 0.82
 
 const atom = (x, y, c) => {
   ctx.fillStyle = c;
@@ -12,6 +16,12 @@ const atom = (x, y, c) => {
 };
 
 function draw() {
+  times = Math.max(100, zoom / 100 * 2);
+  zoomDiv.innerHTML = zoom;
+  translateXDiv.innerHTML = translateX;
+  translateYDiv.innerHTML = translateY;
+  timesDiv.innerHTML = times;
+
   canvas.height = window.innerHeight;
   canvas.width = window.innerWidth;
 
@@ -22,8 +32,8 @@ function draw() {
 
   for (let y = 0; y < dimY; y++) {
     for (let x = 0; x < dimX; x++) {
-      const dx = (x - centerX) / zoom - scaleX;
-      const dy = (y - centerY) / zoom - scaleY;
+      const dx = (x - centerX) / zoom - translateX;
+      const dy = (y - centerY) / zoom - translateY;
 
       let a = dx;
       let b = dy;
@@ -56,4 +66,21 @@ let timer = null;
 window.addEventListener("resize", () => {
   clearTimeout(timer);
   timer = setTimeout(draw, 400);
+});
+
+window.addEventListener("click", (evt) => {
+  translateX += (window.innerWidth / 2 - evt.x) * (1 / zoom);
+  translateY += (window.innerHeight / 2 - evt.y) * (1 / zoom);
+  zoom *= 10;
+  draw();
+});
+window.addEventListener("contextmenu", (evt) => {
+  if (zoom >= 10) {
+    translateX -= (window.innerWidth / 2 - evt.x) * (1 / zoom);
+    translateY -= (window.innerHeight / 2 - evt.y) * (1 / zoom);
+    zoom /= 10;
+    draw();
+  }
+  evt.preventDefault();
+  return false;
 });
